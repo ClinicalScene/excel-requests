31724592ef3e69090824bacab729870fc3eeda74
Bjoern Stiel <bjoern.stiel@pathio.com>
2017-05-19 11:45:22 +0100

    Fixed POST method

--- Requests/Providers/HttpProvider.cs
+++ Requests/Providers/HttpProvider.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using Newtonsoft.Json.Linq;
 using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Requests.Providers
@@ -12,38 +13,37 @@ namespace Requests.Providers
     {
         public JToken Get(string url, Dictionary<string, string> headers)
         {
-            var request = WebRequest.Create(url) as HttpWebRequest;
-            var response = new JObject();
-            request.ProtocolVersion = HttpVersion.Version10;
-            request.KeepAlive = false;
-            request.ServicePoint.Expect100Continue = false;
-            request.UserAgent = "excel-requests";
+            var httpClient = new HttpClient();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
             foreach (var header in headers)
                 request.Headers.Add(header.Key, header.Value);
-            using (var httpResponse = request.GetResponse() as HttpWebResponse)
-            {
-                response.Add("Text", JValue.CreateNull());
-                response.Add("StatusCode", (int)httpResponse.StatusCode);
-                response.Add("StatusDescription", httpResponse.StatusDescription);
-                response.Add("ContentType", httpResponse.ContentType);
-                response.Add("Method", httpResponse.Method);
 
-                if (httpResponse.StatusCode == HttpStatusCode.OK)
-                {
-                    using (var reader = new System.IO.StreamReader(httpResponse.GetResponseStream()))
-                    {
-                        var text = reader.ReadToEnd();
-                        response["Text"] = text;
-                        if (httpResponse.ContentType.ToLower().Contains("application/json"))
-                            response.Add("Json", Parser.Parse(text));
-                    }
-                }
+            var response = httpClient.SendAsync(request).Result;
+            IEnumerable<string> contentTypeAsIEnumerable;
+            response.Content.Headers.TryGetValues("Content-Type", out contentTypeAsIEnumerable);
+            var contentType = contentTypeAsIEnumerable?.First();
+
+            var payload = new JObject();
+
+            payload.Add("Text", JValue.CreateNull());
+            payload.Add("StatusCode", (int)response.StatusCode);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var text = response.Content.ReadAsStringAsync().Result;
+                payload["Text"] = text;
+                if(contentType != null && contentType.ToLower().Contains("application/json"))
+                    payload.Add("Json", Parser.Parse(text));
             }
-            return response;
+            return payload;
         }
 
         public JToken Post(string url, Dictionary<string, string> headers, JToken payload)
         {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
             var request = WebRequest.Create(url) as HttpWebRequest;
             var response = new JObject();
             request.ProtocolVersion = HttpVersion.Version10;
